7138cc0d59b3c25a909c196595bdd230ff9bc28c
winslomk <winslomk@rose-hulman.edu>
2019-03-19 22:23:09 -0400

    Done

--- src/problem4.py
+++ src/problem4.py
@@ -3,8 +3,8 @@ Exam 1, problem 4.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Montgomery Winslow.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -80,12 +80,23 @@ def problem4(point1, point2, n, window):
       :type window:  rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # IMPORTANT: For PARTIAL CREDIT, ignore the colors.
     # -------------------------------------------------------------------------
-
-
+    point1.attach_to(window)
+    point2.attach_to(window)
+    deltax = (point2.x - point1.x) / (2 * n + 1)
+    deltay = (point2.y - point1.y) / (2 * n + 1)
+    for k in range(n):
+        point = rg.Point(point1.x + deltax * (k + 1), point1.y + deltay * (k + 1))
+        point.fill_color = point2.fill_color
+        point.attach_to(window)
+    for k in range(n):
+        point = rg.Point(point1.x + deltax * (k + n + 1), point1.y + deltay * (k + n + 1))
+        point.attach_to(window)
+        point.fill_color = point1.fill_color
+    window.render()
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
